746795251bca691f2d540c60ee5bcb3aca7d36cd
Mukulpatidar <patidarmukul127.com>
2025-12-14 09:09:40 +0530

    Initial commit

---
+++ src/main/java/com/smart/InstagramMiniBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramMiniBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramMiniBackendApplication.class, args);
+	}
+
+}
